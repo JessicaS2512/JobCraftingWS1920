2e491836378b7df9c97c17830cd10d587b94337a
Maike Holle <maikeholle@Maikes-Air-3.fritz.box>
2019-10-31 12:05:48 +0100

    AnalyseScript angelegt

---
+++ AnalyseScript.R
@@ -0,0 +1,10 @@
+# AnalyseScript.R
+## Teamnamen: Job Crafting
+
+# Pakete laden: ----
+
+# Rohdaten laden: ----
+
+# DataCleaning und Skalenberechnung: ----
+
+# Statistische Analyse und Grafiken: ----
